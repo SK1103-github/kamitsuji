8dc87d5a598a0e4689cfc3ead105ea945f16c7b4
Shinichi Kawabata <shin.1987.11.03@gmail.com>
2025-04-11 01:08:48 +0900

    scss構造整理

--- gulpfile.js
+++ gulpfile.js
@@ -23,36 +23,33 @@ gulp.task('sass', function (done) {
     .pipe(plumber()) // エラー防止
     .pipe(sass({ outputStyle: 'expanded', sourceMap: false })) // Dart Sass コンパイル設定
     .pipe(postcss([autoprefixer()])) // CSSに自動プレフィックス付加
-    .pipe(gulp.dest("./assets/css")) // 出力先を assets/css に指定
+    .pipe(gulp.dest("./assets/css")); // 出力先を assets/css に指定
   done();
 });
 
 /**
  * stylus -> cssにコンパイル
  */
-var paths = {
-  stylus: ['./stylus/**/*.styl','!./stylus/**/_*.styl']
-};
-gulp.task('stylus', function(done){
-  gulp.src(paths.stylus)
-  .pipe(stylus({outputStyle: 'expanded'}))
-  .pipe(postcss([autoprefixer()]))
-  .pipe(gulp.dest("./css"));
-  done();
-});
+//var paths = {
+//  stylus: ['./stylus/**/*.styl','!./stylus/**/_*.styl']
+//};
+//gulp.task('stylus', function(done){
+//  gulp.src(paths.stylus)
+//  .pipe(stylus({outputStyle: 'expanded'}))
+//  .pipe(postcss([autoprefixer()]))
+//  .pipe(gulp.dest("./css"));
+//  done();
+//});
 
 /**
  * ファイルの変更・保存を監視
  */
-gulp.task('watch', function(done){
-  gulp.watch('./assets/sass/**/*.scss', gulp.series('sass'));
-  done();
+gulp.task('watch', function() {
+  gulp.watch('./assets/sass/**/*.scss', gulp.series('sass')); // Sassだけを監視
 });
 
 /**
  * Gulpタスクをデフォルトのコマンドで起動
  * $ npx gulp
  */
-gulp.task('default', gulp.series('watch', function(done) {
-  done();
-}));
+gulp.task('default', gulp.series('watch'));
